c0f91ce03a7eaa62233daae0cb2b07062666be18
Christopher Callender <christophercallender0000@gmail.com>
2022-04-27 08:07:41 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+   let form = document.querySelector('form');
+   form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      handleTask(e.target['new-task-description'].value);
+      form.reset();
+   });
 });
+
+function handleTask(todo) {
+   let p = document.createElement('p');
+   let btn = document.createElement('button');
+   btn.addEventListener('click', handleDelete);
+   p.textContent = `${todo} `;
+   btn.textContent = 'Completed';
+   p.appendChild(btn);
+   document.querySelector('#list').appendChild(p);
+}
+
+function handleDelete(e) {
+   e.target.parentNode.remove();
+}
